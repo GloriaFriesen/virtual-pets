a663af2289ed372e90cca3b04de545cfc4a856b0
Chris Finney and Gloria Friesen <chris@>
2017-04-04 16:26:12 -0700

    add Community member variables, getter method, equals override and tests thereof

--- src/main/java/Community.java
+++ src/main/java/Community.java
@@ -4,7 +4,27 @@ public class Community {
   private int id;
 
   public Community(String name, String description) {
-
-
+    this.name = name;
+    this.description = description;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  @Override
+  public boolean equals(Object otherCommunity) {
+    if (!(otherCommunity instanceof Community)) {
+      return false;
+    } else {
+    Community newCommunity = (Community) otherCommunity;
+    return this.getName().equals(newCommunity.getName()) &&
+           this.getDescription().equals(newCommunity.getDescription());
+    }
+  }
+
 }
--- src/test/java/CommunityTest.java
+++ src/test/java/CommunityTest.java
@@ -4,6 +4,7 @@ import org.sql2o.*;
 import java.util.Arrays;
 
 public class CommunityTest {
+  Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters.");
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
@@ -12,5 +13,23 @@ public class CommunityTest {
   public void community_instantiatesCorrectly_true() {
     Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters.");
     assertEquals(true, testCommunity instanceof Community);
+  }
+
+  @Test
+  public void getName_communityInstantiatesWithName_String() {
+    Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters.");
+    assertEquals("Water Enthusiasts", testCommunity.getName());
+  }
+
+  @Test
+  public void getDescription_communityInstantiatesWithDescription_String() {
+    Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters.");
+    assertEquals("Lovers of all things water monsters.", testCommunity.getDescription());
+  }
+
+  @Test
+  public void equals_returnsTrueIfNameAndDescriptionAreTheSame_true() {
+    Community communityTwo = new Community("Water Enthusiasts", "Lovers of all things water monsters.");
+    assertTrue(testCommunity.equals(communityTwo));
   }
 }
