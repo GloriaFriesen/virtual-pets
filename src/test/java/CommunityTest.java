4721444b56a2ca0bac7d71849f576f1a5ea7c5ca
Gloria Friesen <gloriafriesen@gmail.com>
2017-04-04 21:22:34 -0700

    add many-to-many relationship between person and community classes

--- src/test/java/CommunityTest.java
+++ src/test/java/CommunityTest.java
@@ -2,6 +2,8 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import org.sql2o.*;
 import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 
 public class CommunityTest {
   Community testCommunity = new Community("Water Enthusiasts", "Lovers of all things water monsters.");
@@ -31,5 +33,67 @@ public class CommunityTest {
   public void equals_returnsTrueIfNameAndDescriptionAreTheSame_true() {
     Community communityTwo = new Community("Water Enthusiasts", "Lovers of all things water monsters.");
     assertTrue(testCommunity.equals(communityTwo));
+  }
+
+  @Test
+  public void save_insertsObjectIntoDatabase_Community() {
+    testCommunity.save();
+    assertEquals(true, Community.all().get(0).equals(testCommunity));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfCommunity_true() {
+    testCommunity.save();
+    Community secondCommunity = new Community("Fire Enthusiasts", "Flame on");
+    secondCommunity.save();
+    assertEquals(true, Community.all().get(0).equals(testCommunity));
+    assertEquals(true, Community.all().get(1).equals(secondCommunity));
+  }
+
+  @Test
+  public void addPerson_addsPersonToCommunity() {
+    testCommunity.save();
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    testCommunity.addPerson(testPerson);
+    Person savedPerson = testCommunity.getPersons().get(0);
+    assertTrue(testPerson.equals(savedPerson));
+  }
+
+  @Test
+  public void getPersons_returnsAllPersons_List() {
+    testCommunity.save();
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    testCommunity.addPerson(testPerson);
+    List savedPersons = testCommunity.getPersons();
+    assertEquals(savedPersons.size(), 1);
+  }
+
+  @Test
+  public void delete_deletesCommunity_true() {
+    testCommunity.save();
+    testCommunity.delete();
+    assertEquals(0, Community.all().size());
+  }
+
+  @Test
+  public void delete_deletesAllPersonsAndCommunitiesAssociations() {
+    testCommunity.save();
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    testCommunity.addPerson(testPerson);
+    testCommunity.delete();
+    assertEquals(0, testPerson.getCommunities().size());
+  }
+
+  @Test
+  public void removePerson_removesAssociationWithSpecifiedCommunity() {
+    testCommunity.save();
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    testCommunity.removePerson(testPerson);
+    List savedPersons = testCommunity.getPersons();
+    assertEquals(0, savedPersons.size());
   }
 }
